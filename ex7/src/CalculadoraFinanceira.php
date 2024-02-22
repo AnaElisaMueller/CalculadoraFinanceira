c224803880e113a50a78decf0272aa0432b4c76a
AnaElisaMueller <emailanaelisa@gmail.com>
2024-02-22 07:51:39 -0300

    add ex7

---
+++ ex7/src/CalculadoraFinanceira.php
@@ -0,0 +1,147 @@
+<?php
+
+class CalculadoraFinanceira
+{
+    public $jurosSimples = 0;
+    public $jurosCompostos = 0;
+    public $amortizacao = [];
+
+    public function calcularJurosSimples($capital, $taxa, $tempo)
+    {
+        try {
+            if (!isset($capital, $taxa, $tempo)) {
+                throw new Exception("A informação passada não foi definida");
+            }
+
+            if (!is_numeric($capital) || !is_numeric($taxa) || !is_numeric($tempo)) {
+                throw new Exception("A informação passada não é numérica");
+            }
+
+            if ($capital < 0 || $taxa < 0 || $tempo < 0) {
+                throw new Exception("A informação passada não pode ser menor que zero");
+            }
+
+            if (isset($capital, $taxa, $tempo) && is_numeric($capital) && $capital >= 0 && is_numeric($taxa) && $taxa >= 0 && is_numeric($tempo) && $tempo >= 0) {
+                $this->jurosSimples = $capital * $taxa * $tempo;
+            }
+            return round($this->jurosSimples, 2);
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+            echo " - ";
+            echo "ERRO: ";
+            print_r($e->getCode());
+            echo "\n";
+        }
+    }
+
+    public function calcularJurosCompostos($capital, $taxa, $tempo)
+    {
+        try {
+            if (!isset($capital, $taxa, $tempo)) {
+                throw new Exception("A informação passada não foi definida");
+            }
+
+            if (!is_numeric($capital) || !is_numeric($taxa) || !is_numeric($tempo)) {
+                throw new Exception("A informação passada não é numérica");
+            }
+
+            if ($capital < 0 || $taxa < 0 || $tempo < 0) {
+                throw new Exception("A informação passada não pode ser menor que zero");
+            }
+
+            if (isset($capital, $taxa, $tempo) && is_numeric($capital) && $capital >= 0 && is_numeric($taxa) && $taxa >= 0 && is_numeric($tempo) && $tempo >= 0) {
+                $montante = pow((1 + $taxa), $tempo) * $capital;
+                $this->jurosCompostos = $montante - $capital;
+                
+            }
+            return round($this->jurosCompostos, 2);
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+            echo " - ";
+            echo "ERRO: ";
+            print_r($e->getCode());
+            echo "\n";
+        }
+    }
+
+    public function calcularAmortizacao($capital, $taxa, $tempo, $tipo)
+    {
+        try {
+            if (!isset($capital, $taxa, $tempo)) {
+                throw new Exception("A informação passada não foi definida");
+            }
+
+            if (!is_numeric($capital) || !is_numeric($taxa) || !is_numeric($tempo)) {
+                throw new Exception("A informação passada não é numérica");
+            }
+
+            if ($capital < 0 || $taxa < 0 || $tempo < 0) {
+                throw new Exception("A informação passada não pode ser menor que zero");
+            }
+
+            if ($tipo !== "SAC" && $tipo !== "Price") {
+                throw new Exception("A informação passada não não aceita outros tipos");
+            }
+
+            if (isset($capital, $taxa, $tempo) && is_numeric($capital) && $capital >= 0 && is_numeric($taxa) && $taxa >= 0 && is_numeric($tempo) && $tempo >= 0) {
+
+                if ($tipo === "SAC") {
+
+                    $arraySaldoDevedor = [];
+                    $totalJurosSAC = 0;
+                    $saldoDevedor = 0;
+
+                    $amortizacao = $capital / $tempo; 
+                    $saldoDevedor = $capital;
+
+                    for ($linha = 1; $linha <= $tempo; $linha++) {
+                        $jurosSAC = $saldoDevedor * $taxa;
+                        $totalJurosSAC += $jurosSAC;
+                        $saldoDevedor = $saldoDevedor - $amortizacao;
+                        $arraySaldoDevedor[$linha] = $saldoDevedor;
+                    }
+
+                    $this->amortizacao[] = round($totalJurosSAC, 2);
+
+                    for ($linha = 1; $linha <= $tempo; $linha++) {
+                        $this->amortizacao[] = round($arraySaldoDevedor[$linha], 2);
+                    }
+                }
+
+                if ($tipo === "Price") {
+
+                    $saldoDevedor = 0;
+                    $totalJurosPrice = 0;
+                    $arraySaldoDevedor = []; 
+
+                    $parcela = $capital * ((pow((1 + $taxa), $tempo) * $taxa) / (-1 + pow(1 + $taxa, $tempo)));
+                    $saldoDevedor = $capital;
+
+                    for ($linha = 1; $linha <= $tempo; $linha++) {
+
+                        $jurosPrice = $saldoDevedor * $taxa;
+                        $totalJurosPrice += $jurosPrice;
+                        $amortizacaoPrice = $parcela - $jurosPrice;
+                        $saldoDevedor = $saldoDevedor - $amortizacaoPrice;
+                        $arraySaldoDevedor[$linha] = $saldoDevedor;
+                    }
+
+                    $this->amortizacao[] = round($totalJurosPrice, 2);
+
+                    for ($linha = 1; $linha <= $tempo; $linha++) {
+                        $this->amortizacao[] = round($arraySaldoDevedor[$linha], 2);
+                    }
+                }
+            }
+
+            return $this->amortizacao; //array =[total de juros pagos, valor das parcelas];
+
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+            echo " - ";
+            echo "ERRO: ";
+            print_r($e->getCode());
+            echo "\n";
+        }
+    }
+}
---
+++ ex7/tests/CalculadoraFinanceiraTest.php
@@ -0,0 +1,181 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'src/CalculadoraFinanceira.php';
+
+class CalculadoraFinanceiraTest extends TestCase
+{
+    /*
+    Teste para verificar o correto calculo dos métodos somar e subtrair.
+    
+    **Testes principais:
+        *Calcular Juros Simples
+        *Calcular Juros Compostos
+        *Calcular Amortização
+
+    Para cada teste principal, há os seguintes testes:
+
+                  Nome               ||   quantidade por teste principipal
+        *Certo                       ||          1,    1,    2
+        *Extremo                     ||          1,    1,    2     
+        *String                      ||          1,    1,    1
+        *Negativo                    ||          1,    1,    1
+        *TipoIncorreto               ||          0,    0,    1
+        
+    **Exceptions:
+        *Message
+        *Code
+
+    **Resultado de teste:
+        .A informação passada não é numérica - ERRO: 0
+        .A informação passada não pode ser menor que zero - ERRO: 0
+        ..A informação passada não é numérica - ERRO: 0
+        .A informação passada não pode ser menor que zero - ERRO: 0
+        ...A informação passada não é numérica - ERRO: 0
+        .A informação passada não pode ser menor que zero - ERRO: 0
+        .A informação passada não não aceita outros tipos - ERRO: 0
+        .                                                       11 / 11 (100%)
+
+        OK (15 tests, 15 assertions)
+    */
+
+    #Testar Calcular Juros Simples (calcularJurosSimples)
+    public function testCalcularJurosSimples()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosSimples = $classeCalculadoraFinanceira->calcularJurosSimples(1, 0.05, 3);
+
+        $this->assertEquals(0.15, $jurosSimples);
+    }
+
+    public function testCalcularJurosSimplesExtremo()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosSimples = $classeCalculadoraFinanceira->calcularJurosSimples(1700007, 0.05, 22);
+
+        $this->assertEquals(1870007.7, $jurosSimples);
+    }
+
+    public function testCalcularJurosSimplesString()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosSimples = $classeCalculadoraFinanceira->calcularJurosSimples('a', 2, 3);
+
+        $this->assertEquals(0, $jurosSimples);
+    }
+
+    public function testCalcularJurosSimplesNegativo()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosSimples = $classeCalculadoraFinanceira->calcularJurosSimples(-1, -2, 3);
+
+        $this->assertEquals(0, $jurosSimples);
+    }
+
+
+
+    #Testar Calcular Juros Compostos (calcularJurosCompostos)
+    public function testCalcularJurosCompostos() 
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosCompostos = $classeCalculadoraFinanceira->calcularJurosCompostos(1000, 0.08, 5);
+
+        $this->assertEquals(469.33, $jurosCompostos);
+    }
+
+    public function testCalcularJurosCompostosExtremo()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosCompostos = $classeCalculadoraFinanceira->calcularJurosSimples(1700007, 0.05, 22);
+        $this->assertEquals( 1870007.7, $jurosCompostos);
+    }
+
+    public function testCalcularJurosCompostosString()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosCompostos = $classeCalculadoraFinanceira->calcularJurosCompostos('a', 2, 3);
+
+        $this->assertEquals(0, $jurosCompostos);
+    }
+
+    public function testCalcularJurosCompostosNegativo()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $jurosCompostos = $classeCalculadoraFinanceira->calcularJurosCompostos(-1, -2, 3);
+
+        $this->assertEquals(0, $jurosCompostos);
+    }
+
+    #Testar Calcular Amortização (calcularAmortizacao)
+    public function testCalcularAmortizacaoSac() 
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $amortizacao = $classeCalculadoraFinanceira->calcularAmortizacao(20000, 0.05, 10, "SAC");
+
+        $this->assertEquals([5500.0, 18000.0, 16000.0, 14000.0, 12000.0, 10000.0, 8000.0, 6000.0, 4000.0, 2000.0, 0.0], $amortizacao);
+    }
+
+    public function testCalcularAmortizacaoPrice() 
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $amortizacao = $classeCalculadoraFinanceira->calcularAmortizacao(20000, 0.04, 8, "Price");
+
+        $this->assertEquals([3764.45, 17829.44, 15572.06, 13224.39, 10782.81, 8243.57, 5602.75, 2856.3, 0], $amortizacao);
+    }
+
+    public function testCalcularAmortizacaoExtremoSAC()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $amortizacao = $classeCalculadoraFinanceira->calcularAmortizacao(80000000, 0.04, 5, "SAC");
+
+        $this->assertEquals([9600000, 64000000, 48000000, 32000000, 16000000, 0], $amortizacao);
+    }
+
+    public function testCalcularAmortizacaoExtremoPrice()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $amortizacao = $classeCalculadoraFinanceira->calcularAmortizacao(80000000, 0.05, 5, "Price");
+
+        $this->assertEquals([12389919.25, 65522016.15, 50320133.11, 34358155.91, 17598079.86, 0.0], $amortizacao);
+    }
+
+    public function testCalcularAmortizacaoString()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $amortizacao = $classeCalculadoraFinanceira->calcularAmortizacao('a', 2, 3, "Price");
+
+        $this->assertEquals(0, $amortizacao);
+    }
+
+    public function testCalcularAmortizacaoNegativo()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $amortizacao = $classeCalculadoraFinanceira->calcularAmortizacao(-1, -2, 3, "Price");
+
+        $this->assertEquals(0, $amortizacao);
+    }
+
+    public function testCalcularAmortizacaoTipo()
+    {
+
+        $classeCalculadoraFinanceira = new CalculadoraFinanceira;
+        $amortizacao = $classeCalculadoraFinanceira->calcularAmortizacao(1, 2, 3, "NENHUM");
+
+        $this->assertEquals(0, $amortizacao);
+    }
+}
